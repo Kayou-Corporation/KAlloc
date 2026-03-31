d3712f00cf61e2fb4e3ae56bea2d8392178c5183
Ethan Ruhlmann <e.ruhlmann@student.isartdigital.com>
2026-03-31 16:34:20 +0200

    [ADD] MemoryAllocator class main definitions

--- Code/Include/MemoryAllocator.hpp
+++ Code/Include/MemoryAllocator.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
+#include <cstddef>
+
+
+
 namespace Kayou
 {
     class MemoryAllocator
     {
     public:
-        MemoryAllocator() = default;
-        ~MemoryAllocator() = default;
+        virtual ~MemoryAllocator() = default;
+        virtual void* alloc(std::size_t size) = 0;
+        virtual void free(void* ptr, std::size_t size) = 0;
+        virtual std::size_t GetTotalAllocatedSize() const = 0;
+        virtual void PrintUsage() const = 0;
+
+
     };
 }
--- Code/Source/MemoryAllocator.cpp
+++ Code/Source/MemoryAllocator.cpp
@@ -2,4 +2,4 @@
 
 namespace Kayou
 {
-} // Kayou
+}
